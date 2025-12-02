194b13c09517dcb0b13de2b675606dd51329fe96
Garason <Andrew.Garason@gmail.com>
2025-12-02 17:56:43 -0500

    added survey handling for send submit

--- server.js
+++ server.js
@@ -2,8 +2,20 @@ const express = require('express');
 const app = express();
 const PORT = 3000;
 
+// Middleware: allow Express to read form data (from <form> submits)
+app.use(express.urlencoded({ extended: true }));
+
 app.get('/', (req, res) => {
   res.send('Server is working 🎯');
+});
+
+// This route will handle your survey form submissions
+app.post('/submit-survey', (req, res) => {
+  console.log('✅ Survey submission received:');
+  console.log(req.body); // This will contain your form fields
+
+  // Send a simple response back to the browser
+  res.send('Thanks for submitting the survey!');
 });
 
 app.listen(PORT, () => {
