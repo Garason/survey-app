a944979d144a9523c08cfd2cd0eceef3866ab80c
Garason <Andrew.Garason@gmail.com>
2025-12-02 18:03:37 -0500

    adjust syntax

--- server.js
+++ server.js
@@ -2,19 +2,15 @@ const express = require('express');
 const app = express();
 const PORT = 3000;
 
-// Middleware: allow Express to read form data (from <form> submits)
 app.use(express.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
   res.send('Server is working 🎯');
 });
 
-// This route will handle your survey form submissions
 app.post('/submit-survey', (req, res) => {
   console.log('✅ Survey submission received:');
-  console.log(req.body); // This will contain your form fields
-
-  // Send a simple response back to the browser
+  console.log(req.body);
   res.send('Thanks for submitting the survey!');
 });
 
